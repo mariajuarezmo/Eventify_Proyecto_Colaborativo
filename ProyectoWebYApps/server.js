cd0ef6993ed679f085543f2a6e6d166a527d2123
Maria Juarez <200039@iesmargaritasalas.com>
2024-12-14 11:59:55 +0100

    cambio port por PORT

--- ProyectoWebYApps/server.js
+++ ProyectoWebYApps/server.js
@@ -34,7 +34,7 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage });
 const options = {
     host: process.env.DB_HOST,
-    PORT: process.env.DB_PORT,
+    port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
@@ -121,7 +121,7 @@ app.get('/', (req, res) => {
 // Configuración de conexión a la base de datos
 const con = mysql.createConnection({
     host: process.env.DB_HOST,
-    PORT: process.env.DB_PORT,
+    port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
